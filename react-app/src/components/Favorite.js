33a5fe6e66646c00688e874e1e4b064af7b2404d
NikitaThomas342 <nikita_tho@cmu.ac.th>
2022-05-09 23:25:50 +0700

    test

--- react-app/src/components/Favorite.js
+++ react-app/src/components/Favorite.js
@@ -53,7 +53,7 @@ function Favorite ({ className }) {
                             e.preventDefault()
                             Swal.fire({
                               title: 'Are you sure?',
-                              text: "You won't be able to revert this!!",
+                              text: "You won't be able to revert this!",
                               icon: 'warning',
                               showCancelButton: true,
                               confirmButtonColor: '#3085d6',
